f8794d983e430e784e3accd338a80dc6410b7ace
AleksandarV91 <s.pilence@gmail.com>
2022-01-09 14:55:52 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const btn = document.querySelector(".cta-btn");
+const emailInput = document.querySelector("input");
+const err = document.querySelector("small");
+
+const isValid = (email) => {
+  let regEx =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regEx.test(String(email).toLocaleLowerCase());
+};
+
+btn.addEventListener("click", () => {
+  const emailValue = emailInput.value.trim();
+  if (emailValue === "" || !isValid(emailValue)) {
+    err.innerHTML = "Error! Please check your email";
+  } else {
+    emailInput.value = "";
+    err.innerHTML = "";
+  }
+});
